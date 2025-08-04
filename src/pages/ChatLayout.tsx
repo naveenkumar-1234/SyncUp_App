1f6788ee751ff4d32d4e2463821b8c2b4d609226
naveenkumar-1234 <iamnaveenkumar23@gmail.com>
2025-08-04 18:05:48 +0530

    (chore): unused component removed

--- src/pages/ChatLayout.tsx
+++ src/pages/ChatLayout.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '@/context/AuthContext'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback} from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { ScrollArea } from '@/components/ui/scroll-area'
